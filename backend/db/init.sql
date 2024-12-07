13336980d9f9f6137cd036972656cf8cd4cd6497
Carter Hanson <carter.hanson@jacks.sdstate.edu>
2024-12-06 18:25:39 -0600

    Remove backend folder, frontend folder, and docker-compose.yml

--- backend/db/init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS tasks (
-  id SERIAL PRIMARY KEY,
-  title VARCHAR(255) NOT NULL,
-  description TEXT NOT NULL,
-  due_date DATE NOT NULL,
-  priority VARCHAR(50) NOT NULL,
-  status VARCHAR(50) NOT NULL
-);
